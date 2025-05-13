88564b131c9d4803e9349808a0d9e17396d8c326
Sebastian Stammler <sebastian.stammler@gmail.com>
2025-05-13 03:37:44 +0200

    Initial commit

---
+++ paulaner_colormaps/__init__.py
@@ -0,0 +1,40 @@
+from matplotlib.colors import LinearSegmentedColormap as _lsc
+
+spezi = _lsc.from_list(
+    "spezi",
+    [
+        "#4b1e7d",
+        "#e30083",
+        "#e6001c",
+        "#ec6400",
+        "#f9b700",
+    ],
+)
+
+orange = _lsc.from_list(
+    "orange",
+    [
+        "#eb0029",
+        "#ff4713",
+        "#ff6c0d",
+        "#ff9e19",
+        "#ffc600",
+    ],
+)
+
+zitrone = _lsc.from_list(
+    "zitrone",
+    [
+        "#00a441",
+        "#6fb634",
+        "#b1bf14",
+        "#d4c31e",
+        "#fcc600",
+    ],
+)
+
+__all__ = [
+    orange,
+    spezi,
+    zitrone,
+]
